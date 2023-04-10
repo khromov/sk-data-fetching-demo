f7cea88b0c9d4da18777698db1b233dc3e131deb
Stanislav Khromov <stanislav.khromov@gmail.com>
2023-04-10 21:09:15 +0200

    Repro

--- src/routes/images/[[id]]/+page.svelte
+++ src/routes/images/[[id]]/+page.svelte
@@ -6,19 +6,19 @@
 
 <ul>
     <li>
-        <a href="/images?q=1">Image 1</a>
+        <a data-sveltekit-preload-data="tap" href="/images?q=1">Image 1</a>
     </li>
     <li>
-        <a href="/images?q=2">Image 2</a>
+        <a data-sveltekit-preload-data="tap" href="/images?q=2">Image 2</a>
     </li>
     <li>
-        <a href="/images?q=3">Image 3</a>
+        <a data-sveltekit-preload-data="tap" href="/images?q=3">Image 3</a>
     </li>
     <li>
-        <a href="/images?q=4">Image 4</a>
+        <a data-sveltekit-preload-data="tap" href="/images?q=4">Image 4</a>
     </li>
     <li>
-        <a href="/images?q=5">Image 5</a>
+        <a data-sveltekit-preload-data="tap" href="/images?q=5">Image 5</a>
     </li>
 </ul>
 
